abe4c374e142af36cc7f0bc5cdc5ede67abcb03e
jdoh <98573005+dandb3@users.noreply.github.com>
2023-07-04 05:54:51 +0000

    delete: construct/destruct/function msg

--- ex03/Fixed.cpp
+++ ex03/Fixed.cpp
@@ -4,49 +4,38 @@
 
 Fixed::Fixed(void)
 : _fixedPointNum(0)
-{
-	std::cout << "Default constructor called" << std::endl;
-}
+{}
 
 Fixed::Fixed(const int num)
 {
-	std::cout << "Int constructor called" << std::endl;
 	this->_fixedPointNum = (num << this->_fractionalBits);
 }
 
 Fixed::Fixed(const float num)
 {
-	std::cout << "Float constructor called" << std::endl;
 	this->_fixedPointNum = static_cast<int>(roundf(num * (1 << this->_fractionalBits)));
 }
 
 Fixed::Fixed(const Fixed& fixed)
 : _fixedPointNum(fixed._fixedPointNum)
-{
-	std::cout << "Copy constructor called" << std::endl;
-}
+{}
 
 Fixed::~Fixed(void)
-{
-	std::cout << "Destructor called" << std::endl;
-}
+{}
 
 Fixed& Fixed::operator=(const Fixed& fixed)
 {
-	std::cout << "Copy assignment operator called" << std::endl;
 	this->_fixedPointNum = fixed._fixedPointNum;
 	return *this;
 }
 
 int Fixed::getRawBits(void) const
 {
-	std::cout << "getRawBits member function called" << std::endl;
 	return this->_fixedPointNum;
 }
 
 void Fixed::setRawBits(int const raw)
 {
-	std::cout << "setRawBits member function called" << std::endl;
 	this->_fixedPointNum = raw;
 }
 
